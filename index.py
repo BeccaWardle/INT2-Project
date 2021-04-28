34d6cc08c6646a40b327b7cc66cfbd5ff04f41bb
Alex V <2038611+lexffe@users.noreply.github.com>
2021-04-28 11:25:37 +0100

    Working network

--- index.py
+++ index.py
@@ -1,40 +1,143 @@
-# https://pytorch.org/vision/stable/datasets.html#torchvision.datasets.CIFAR10
+## Dataset (in PyTorch)
+## https://pytorch.org/vision/stable/datasets.html#torchvision.datasets.CIFAR10
 
-
-## Imports
+## Dataset homepage
+## https://www.cs.toronto.edu/~kriz/cifar.html
 
 #%%
+## Imports
+
 import torch
+from torch import nn
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor, Lambda, Compose
-from torchvision import datasets
+from torchvision import datasets, utils, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import network
 
-## load data (download data from UToronto)
+
 #%%
+## Hardware acceleration
+
+torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+## normalise the data
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+#%%
+## load data (download data from UToronto)
 training_data = CIFAR10(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor(),
+    transform=transform,
 )
 
 test_data = CIFAR10(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor(),
+    transform=transform,
 )
 
-## Test run: load
+## load the dataset, describe the shape
 #%%
 
 batch_size = 64
 
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+# image is of 32x32 with 3 channel for colours
+# batch size is 64, but can be modified
 
 for X, y in test_dataloader:
-    print("Shape of X [N, C, H, W]: ", X.shape)
-    print("Shape of y: ", y.shape, y.dtype)
+
+    print("Shape of X [n, Channels, Height, Width]: ", X.shape, X.dtype)
+    print("Shape of y: ", y.shape, y.dtype) # classification
     break
+
+#%%
+## Visualisation
+
+# train_features, train_labels = next(iter(train_dataloader))
+# print(f"Feature batch shape: {train_features.size()}")
+# print(f"Labels batch shape: {train_labels.size()}")
+# img = train_features[0].squeeze().permute(1,2,0)
+# label = train_labels[0]
+# plt.imshow(img)
+# plt.show()
+# print(f"Label: {label}")
+
+# %%
+
+network_model = network.Network()
+network_model.to(device) # send tensors to CUDA cores
+print(network_model)
+
+# logits = network_model(train_dataloader)
+# pred_probab = nn.Softmax(dim=1)(logits)
+
+## define hyper-parameters
+
+batch_size = 64
+learning_rate = 1e-3
+
+cross_entropy_loss = nn.CrossEntropyLoss()
+stochastic_GD = torch.optim.SGD(network_model.parameters(), lr=learning_rate)
+
+# training
+
+def train_loop(dataloader, model:nn.Module, loss_fn, optimiser: torch.optim.Optimizer):
+
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+
+        X, y = X.to(device), y.to(device) # send data to device
+
+        # predict
+        pred = model(X)
+
+        # compare prediction against actual
+        loss = loss_fn(pred, y)
+
+        # back_prop
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model:nn.Module, loss_fn):
+
+    size = len(dataloader.dataset)
+    test_loss, correct = 0,0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= size
+    correct /= size
+
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, network_model, cross_entropy_loss, stochastic_GD)
+    test_loop(test_dataloader, network_model, cross_entropy_loss)
+print("Done!")
+
+torch.save(network_model.state_dict(), "model.pth")
---
+++ network.py
@@ -0,0 +1,27 @@
+from torch.nn import \
+  Conv2d, ReLU, Linear, MaxPool2d, Module, Flatten, Sequential
+
+
+class Network(Module):
+  def __init__(self):
+    super(Network, self).__init__()
+
+    self.pool = MaxPool2d(2)  # 2*2 max pooling
+
+    self.cnn_relu_stack = Sequential(
+      Conv2d(3, 16, 3),  # 3 in-channel, 16 out-channel
+      ReLU(),
+      self.pool,
+      Conv2d(16, 32, 3),
+      ReLU(),
+      self.pool,
+      Flatten(),
+      Linear(32 * 6 * 6, 160),
+      ReLU(),
+      Linear(160, 84),
+      ReLU(),
+      Linear(84,10),  # 10 classes, final output
+    )
+
+  def forward(self, x):
+    return self.cnn_relu_stack(x)
