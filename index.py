a53f9780c154a851b969471fcbe0b2301334c4a7
Rebecca Wardle <mattydwardle@gmail.com>
2021-04-29 13:09:23 +0100

    Formatted document according to my linters, sorry Brian

--- index.py
+++ index.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python3
-## Dataset (in PyTorch)
-## https://pytorch.org/vision/stable/datasets.html#torchvision.datasets.CIFAR10
+# Dataset (in PyTorch)
+# https://pytorch.org/vision/stable/datasets.html#torchvision.datasets.CIFAR10
 
-## Dataset homepage
-## https://www.cs.toronto.edu/~kriz/cifar.html
+# Dataset homepage
+# https://www.cs.toronto.edu/~kriz/cifar.html
 
-#%%
-## Imports
+# %%
+# Imports
+
+import atexit
+import datetime
+from time import time
 
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
-import network
-import datetime
-import time
 
-script_start = time.time()
+import network
+
+script_start = time()
 print(f"Started: {datetime.datetime.now()}")
 
-## continue training
+# continue training
 cont = False
 
-#%%
-## Hardware acceleration
+# %%
+# Hardware acceleration
 
 torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-## normalise the data
+# normalise the data
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-#%%
-## load data (download data from UToronto)
+# %%
+# load data (download data from UToronto)
 training_data = CIFAR10(
     root="data",
     train=True,
@@ -50,8 +53,8 @@ test_data = CIFAR10(
     transform=transform,
 )
 
-## load the dataset, describe the shape
-#%%
+# load the dataset, describe the shape
+# %%
 
 batch_size = 64
 
@@ -64,11 +67,11 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 for X, y in test_dataloader:
 
     print("Shape of X [n, Channels, Height, Width]: ", X.shape, X.dtype)
-    print("Shape of y: ", y.shape, y.dtype) # classification
+    print("Shape of y: ", y.shape, y.dtype)  # classification
     break
 
-#%%
-## Visualisation
+# %%
+# Visualisation
 
 # train_features, train_labels = next(iter(train_dataloader))
 # print(f"Feature batch shape: {train_features.size()}")
@@ -83,19 +86,19 @@ for X, y in test_dataloader:
 
 network_model = network.Network()
 
-## continue training -> load previous model
-if cont == True:
+# continue training -> load previous model
+if cont:
     print("continuing previous progress.")
     network_model.load_state_dict(torch.load("model.pth"))
     network_model.eval()
 
-network_model.to(device) # send tensors to CUDA cores
+network_model.to(device)  # send tensors to CUDA cores
 print(network_model)
 
 # logits = network_model(train_dataloader)
 # pred_probab = nn.Softmax(dim=1)(logits)
 
-## define hyper-parameters
+# define hyper-parameters
 
 batch_size = 64
 learning_rate = 1e-3
@@ -105,13 +108,14 @@ stochastic_GD = torch.optim.SGD(network_model.parameters(), lr=learning_rate)
 
 # training
 
-def train_loop(dataloader, model:nn.Module, loss_fn, optimiser: torch.optim.Optimizer):
 
-    iteration_start = time.time()
+def train_loop(dataloader, model: nn.Module, loss_fn, optimiser: torch.optim.Optimizer):
+
+    iteration_start = time()
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
 
-        X, y = X.to(device), y.to(device) # send data to device
+        X, y = X.to(device), y.to(device)  # send data to device
 
         # predict
         pred = model(X)
@@ -129,13 +133,13 @@ def train_loop(dataloader, model:nn.Module, loss_fn, optimiser: torch.optim.Opti
             print(f"Loss: {loss:>7f} [{current:>5d}/{size:>5d}]", sep="", end="\r", flush=True)
 
     print()
-    print(f"time since start: {time.time() - script_start:>0.2f}s, time since iteration start: {time.time() - iteration_start:>0.2f}s \n")
+    print(f"time since start: {time() - script_start:>0.2f}s, time since iteration start: {time() - iteration_start:>0.2f}s \n")
 
 
-def test_loop(dataloader, model:nn.Module, loss_fn):
+def test_loop(dataloader, model: nn.Module, loss_fn):
 
     size = len(dataloader.dataset)
-    test_loss, correct = 0,0
+    test_loss, correct = 0, 0
 
     with torch.no_grad():
         for X, y in dataloader:
@@ -150,6 +154,7 @@ def test_loop(dataloader, model:nn.Module, loss_fn):
     print(f"Accuracy: {(100 * correct):>0.2f}%, Avg loss: {test_loss:>8f}\n")
 
     return correct
+
 
 epochs = 350
 max_accuracy = 0
@@ -167,16 +172,15 @@ for t in range(epochs):
         consecutive += 1
 
     if consecutive == max_consecutive:
-        print(f"model reached max potential, stopping.")
+        print("model reached max potential, stopping.")
         print(f"max accuracy: {(100 * correct):>0.2f}%")
-        print(f"time since start: {time.time() - script_start:>0.2f}s")
+        print(f"time since start: {time() - script_start:>0.2f}s")
         break
 
 print("Done!")
 
-## save model state
+# save model state
 torch.save(network_model.state_dict(), "model.pth")
 
-# also save state when Ctrl-C
-import atexit
+# save state when Ctrl-C
 atexit.register(torch.save, network_model.state_dict(), "model.pth")
