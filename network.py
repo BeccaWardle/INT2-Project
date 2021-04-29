a147ccf9618d7310d0fe201048f01c4c885ebb9e
Alex V <2038611+lexffe@users.noreply.github.com>
2021-04-29 11:18:44 +0100

    network modification

--- network.py
+++ network.py
@@ -2,26 +2,29 @@
 from torch.nn import \
   Conv2d, ReLU, Linear, MaxPool2d, Module, Flatten, Sequential
 
-
 class Network(Module):
+
   def __init__(self):
     super(Network, self).__init__()
 
     self.pool = MaxPool2d(2)  # 2*2 max pooling
 
     self.cnn_relu_stack = Sequential(
-      Conv2d(3, 16, 3),  # 3 in-channel, 16 out-channel
+      Conv2d(3, 16, 3),  # 3 in-channel, 16 out-channel, number of kernel
       ReLU(),
       self.pool,
-      Conv2d(16, 32, 3),
+      Conv2d(16, 24, 4),
+      ReLU(),
+      self.pool,
+      Conv2d(24, 56, 3),
       ReLU(),
       self.pool,
       Flatten(),
-      Linear(32 * 6 * 6, 160),
+      Linear(224, 64),
       ReLU(),
-      Linear(160, 84),
+      Linear(64, 48),
       ReLU(),
-      Linear(84,10),  # 10 classes, final output
+      Linear(48, 10),  # 10 classes, final output
     )
 
   def forward(self, x):
