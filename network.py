6e1c7631304e41faa237d36cf2dd03f77e928e10
Alex V <2038611+lexffe@users.noreply.github.com>
2021-05-06 09:32:49 +0100

    1.7

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ class Network(Module):
 
   def __init__(self):
     super(Network, self).__init__()
-    self.__version__ = "1.6"
+    self.__version__ = "1.7"
 
     # self.pool = MaxPool2d(2)  # 2*2 max pooling
 
@@ -16,42 +16,42 @@ class Network(Module):
       # Conv Layer block 1 -- feature extraction
       Conv2d(3, 32, 3, 1),
       BatchNorm2d(32),
-      LeakyReLU(inplace=True),
+      ReLU(inplace=True),
       Conv2d(32, 128, 3, 1), # 64
-      LeakyReLU(inplace=True),
+      ReLU(inplace=True),
       MaxPool2d(2, 2),
-      Dropout2d(p=0.16),
+      Dropout2d(p=0.1),
 
       # Conv Layer block 2
       Conv2d(128, 128, 3, 1),
       BatchNorm2d(128),
-      LeakyReLU(inplace=True),
+      ReLU(inplace=True),
       Dropout2d(p=0.16),
       Conv2d(128, 256, 3, 1),
-      LeakyReLU(inplace=True),
+      ReLU(inplace=True),
       MaxPool2d(3, 2),
       Dropout2d(p=0.2),
 
       # Conv Layer block 3
       Conv2d(256, 256, 3, 1),
       BatchNorm2d(256),
-      LeakyReLU(inplace=True),
+      ReLU(inplace=True),
       Dropout2d(p=0.2),
-      Conv2d(256, 512, 3, 1),
-      LeakyReLU(inplace=True),
+      Conv2d(256, 384, 3, 1),
+      ReLU(inplace=True),
       MaxPool2d(3, 2),
       Dropout2d(p=0.16),
 
       Flatten(),
 
-      Dropout(p=0.16),
-      Linear(4608, 2048),
+      Dropout(p=0.05),
+      Linear(3456, 2560),
       LeakyReLU(inplace=True),
-      Dropout(p=0.08),
-      Linear(2048, 512),
+      Dropout(p=0.25),
+      Linear(2560, 384),
       LeakyReLU(inplace=True),
-      Dropout(p=0.16),
-      Linear(512, 10),
+      Dropout(p=0.2),
+      Linear(384, 10),
       # softmax (?)
     )
 
