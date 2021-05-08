10e441d9952c1860c9be21b28eb3b7064f077ed0
Zijun-Zou <72925964+Zijun-Zou@users.noreply.github.com>
2021-05-08 20:38:52 +0800

    re

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from torch.nn import \
-  Conv2d, ReLU, Linear, MaxPool2d, Module, Flatten, Sequential, BatchNorm2d, Dropout, Dropout2d
+  Conv2d, ReLU, Linear, MaxPool2d, Module, Flatten, Sequential, BatchNorm2d, Dropout, Dropout2d, LeakyReLU
 
 
 class Network(Module):
@@ -8,56 +8,58 @@ class Network(Module):
     super(Network, self).__init__()
 
     self.conv_layer = Sequential(
-
       # Conv Layer block 1
-      #32x32x3 --> 32x32x32
       Conv2d(3, 32, 3, 1),
       BatchNorm2d(32),
-      #32x32x32 --> 32x32x64
-      ReLU(inplace=True),
-      Conv2d(32, 64, 3, 1),
-      ReLU(inplace=True),
-      #32x32x64 --> 16x16x64
+      LeakyReLU(inplace=True),
+      Conv2d(32, 128, 3, 1), # 64
+      LeakyReLU(inplace=True),
       MaxPool2d(2, 2),
-      Dropout(p=0.3),
+      Dropout2d(p=0.16),
 
       # Conv Layer block 2
-      #16x16x64 --> 16x16x128
-      Conv2d(64, 128, 3, 1),
-      BatchNorm2d(128),
-      ReLU(inplace=True),
-      #16x16x128
-      Conv2d(128, 128, 3, 1),
-      ReLU(inplace=True),
-      #8x8x128
+      Conv2d(128, 192, 3, 1),
+      BatchNorm2d(192),
+      LeakyReLU(inplace=True),
+      Dropout2d(p=0.16),
+      Conv2d(192, 384, 3, 1),
+      LeakyReLU(inplace=True),
       MaxPool2d(2, 2),
-      Dropout2d(p=0.05),
+      Dropout2d(p=0.2),
       # Dropout(p=0.2),
 
       # Conv Layer block 3
-      #8x8x128 --> 8x8x256
-      Conv2d(128, 256, 3, 1),
-      BatchNorm2d(256),
-      ReLU(inplace=True),
-      #8x8x256 --> 8x8x256
-      Conv2d(256, 256, 3, 1),
-      ReLU(inplace=True),
-      #4x4x256
-      MaxPool2d(2, 2),
-      Dropout(p=0.3)
-    )
+      Conv2d(384, 512, 3, 1),
+      BatchNorm2d(512),
+      LeakyReLU(inplace=True),
+      Dropout2d(p=0.2),
+      Conv2d(512, 1024, 3, 1),
+      LeakyReLU(inplace=True),
+      BatchNorm2d(1024),
+      Dropout2d(p=0.16),
 
+      Conv2d(1024, 1024, 3, 1),
+      LeakyReLU(inplace=True),
+      MaxPool2d(3, 2),
+      Dropout2d(p=0.16),
 
-    self.fc_layer = Sequential(
+      Flatten(),
+
+      Dropout(p=0.16),
+      Linear(9216, 4096),
+      LeakyReLU(inplace=True),
+      Linear(4096, 2048),
+      LeakyReLU(inplace=True),
       Dropout(p=0.1),
-      Linear(4096, 1024),
-      ReLU(inplace=True),
-      Linear(1024, 512),
-      ReLU(inplace=True),
-      Dropout(p=0.3),
-      Linear(512, 10)
+      Linear(2048, 512),
+      LeakyReLU(inplace=True),
+      Dropout(p=0.16),
+      Linear(512, 10),
+      # softmax (?)
     )
 
+      ## Conv Layer block 1
+  
 
   def forward(self, x):
     """Perform forward."""
