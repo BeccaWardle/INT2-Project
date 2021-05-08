dbb8b75b78d9dcd292f6da294d1a4eac16e25fab
Alex V <2038611+lexffe@users.noreply.github.com>
2021-05-08 16:20:36 +0100

    1.22.1 leakyrelu

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ class Network(Module):
 
   def __init__(self):
     super(Network, self).__init__()
-    self.__version__ = "1.22"
+    self.__version__ = "1.22.1"
 
     # self.pool = MaxPool2d(2)  # 2*2 max pooling
 
@@ -16,29 +16,29 @@ class Network(Module):
       # Conv Layer block 1 -- feature extraction
       Conv2d(3, 32, 3, 1),
       BatchNorm2d(32),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       Conv2d(32, 128, 3, 1), # 64
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(2, 2),
       Dropout2d(p=0.25),
 
       # Conv Layer block 2
       Conv2d(128, 256, 3, 1),
       BatchNorm2d(256),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       Dropout(p=0.4),
       Conv2d(256, 256, 3, 1),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(4, 2),
       Dropout2d(p=0.35),
 
       # Conv Layer block 3
       Conv2d(256, 384, 3, 1),
       BatchNorm2d(384),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       Dropout(p=0.25),
       Conv2d(384, 384, 3, 1),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(4, 2),
       Dropout2d(p=0.375),
 
