81500dc6aec3317c8292455451a36bf0ca07296c
Alex V <2038611+lexffe@users.noreply.github.com>
2021-05-10 13:19:52 +0100

    try without resizing image

--- network.py
+++ network.py
@@ -7,45 +7,45 @@ class Network(Module):
 
   def __init__(self):
     super(Network, self).__init__()
-    self.__version__ = "1.22"
+    self.__version__ = "1.22-optim-var-2"
 
     # self.pool = MaxPool2d(2)  # 2*2 max pooling
 
     self.cnn_relu_stack = Sequential(
 
       # Conv Layer block 1 -- feature extraction
-      Conv2d(3, 32, 3, 1),
+      Conv2d(3, 16, 3, 1),
       LeakyReLU(inplace=True),
-      BatchNorm2d(32),
-      Conv2d(32, 128, 3, 1), # 64
+      BatchNorm2d(16),
+      Conv2d(16, 16, 3, 1), # 64
       LeakyReLU(inplace=True),
-      MaxPool2d(2, 2),
+      # MaxPool2d(2, 2),
       Dropout2d(p=0.25),
 
       # Conv Layer block 2
-      Conv2d(128, 256, 3, 1),
+      Conv2d(16, 64, 3, 1),
       LeakyReLU(inplace=True),
-      BatchNorm2d(256),
+      BatchNorm2d(64),
       Dropout(p=0.4),
-      Conv2d(256, 256, 3, 1),
+      Conv2d(64, 64, 3, 1),
       LeakyReLU(inplace=True),
-      MaxPool2d(4, 2),
+      MaxPool2d(2, 2),
       Dropout2d(p=0.35),
 
       # Conv Layer block 3
-      Conv2d(256, 384, 3, 1),
+      Conv2d(64, 128, 3, 1),
       LeakyReLU(inplace=True),
-      BatchNorm2d(384),
-      Dropout(p=0.25),
-      Conv2d(384, 384, 3, 1),
+      BatchNorm2d(128),
+      Dropout(p=0.3),
+      Conv2d(128, 128, 3, 1),
       LeakyReLU(inplace=True),
-      MaxPool2d(4, 2),
+      MaxPool2d(3, 2),
       Dropout2d(p=0.375),
 
       Flatten(),
 
       Dropout(p=0.2),
-      Linear(3456, 1024),
+      Linear(1152, 1024),
       LeakyReLU(inplace=True),
       Dropout(p=0.4),
       Linear(1024, 128),
