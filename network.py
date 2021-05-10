857203057b79c9f4cf6ec156c1ffe0473865adc6
Alex V <2038611+lexffe@users.noreply.github.com>
2021-05-10 10:48:44 +0100

    reordering layer (BatchNorm2d)

--- network.py
+++ network.py
@@ -15,30 +15,30 @@ class Network(Module):
 
       # Conv Layer block 1 -- feature extraction
       Conv2d(3, 32, 3, 1),
+      LeakyReLU(inplace=True),
       BatchNorm2d(32),
-      ReLU(inplace=True),
       Conv2d(32, 128, 3, 1), # 64
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(2, 2),
       Dropout2d(p=0.25),
 
       # Conv Layer block 2
       Conv2d(128, 256, 3, 1),
+      LeakyReLU(inplace=True),
       BatchNorm2d(256),
-      ReLU(inplace=True),
       Dropout(p=0.4),
       Conv2d(256, 256, 3, 1),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(4, 2),
       Dropout2d(p=0.35),
 
       # Conv Layer block 3
       Conv2d(256, 384, 3, 1),
+      LeakyReLU(inplace=True),
       BatchNorm2d(384),
-      ReLU(inplace=True),
       Dropout(p=0.25),
       Conv2d(384, 384, 3, 1),
-      ReLU(inplace=True),
+      LeakyReLU(inplace=True),
       MaxPool2d(4, 2),
       Dropout2d(p=0.375),
 
